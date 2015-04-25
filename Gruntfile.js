edfa80f08de346c051cc284ba5c35129f406f132
Dmitriy Kazinov <dmitriy.kazinov@>
2015-04-25 15:20:09 +0300

    grunt setup + client dependencies

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,11 +1,35 @@
 /*global module:false*/
 module.exports = function(grunt) {
 
+  var tsSrc = ['**/*.ts', "!node_modules/**/*.ts"];
   // Project configuration.
   grunt.initConfig({
+    watch: {
+      ts: {
+        files: tsSrc,
+        tasks: ['ts', 'tslint:src']
+      }
+    },
+    ts: {
+      default : {
+        src: tsSrc
+      }
+    },
+    tslint: {
+      options: {
+        configuration: grunt.file.readJSON("tslint.json")
+      },
+      src: [
+        tsSrc
+      ]
+    }
   });
 
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks("grunt-ts");
+  grunt.loadNpmTasks('grunt-tslint');
+
   // Default task.
-  grunt.registerTask('default', []);
+  grunt.registerTask('default', ["ts"]);
 
 };
---
+++ src/client/app.ts
@@ -0,0 +1,8 @@
+/**
+ * Created by user on 25.04.2015.
+ */
+module Sgcc {
+    'use strict';
+
+    var test: number = 777;
+}
